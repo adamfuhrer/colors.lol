6190eba7287a5ff632cbf3690b7b33f0896a6d15
adamfuhrer <adam.fuhrer@caseware.com>
2020-04-06 22:32:37 -0400

    rename JSON file, unsubscribe color filter subscription

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,35 +1,36 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {Component, HostBinding, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Title} from '@angular/platform-browser';
 import {Color} from '../color-palette/color-palette.component';
 import {ColorFilterService} from '../../services/color-filter.service';
+import {Subscription} from 'rxjs';
 
 import * as chroma from 'chroma-js';
-import colors from '../../colors.json';
+import palettes from '../../palettes.json';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit, OnDestroy {
   @HostBinding('class.is-showing-single-palette') paletteParam: string;
   palettes = [];
   unfilteredPalettes = [];
+  colorFilterSubscription: Subscription;
 
   constructor(private route: ActivatedRoute,
               private colorFilterService: ColorFilterService,
               private titleService: Title) {
-    // Parse JSON
-    const palettes = [];
-    for (const key in colors) {
-      palettes.push(colors[key]);
+    const pals = [];
+    for (const key in palettes) {
+      pals.push(palettes[key]);
     }
-    this.palettes = palettes[0];
+    this.palettes = pals[0];
     this.unfilteredPalettes = this.palettes;
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.paletteParam = params.get('id');
 
@@ -40,10 +41,11 @@ export class HomepageComponent implements OnInit {
       }
     });
 
-    this.colorFilterService.filterColor.subscribe(color => {
+    this.colorFilterSubscription = this.colorFilterService.filterColor.subscribe(color => {
       if (color) {
         this.palettes = this.unfilteredPalettes;
-        // Compare the filter color to the colours of each palette. If there is a similar match, show the color in list of filtered results
+        // Compare the filter color to the colours of each palette
+        // If there is a similar match, show the color in list of filtered results
         this.palettes = this.palettes.filter(palette => {
           return palette.some(c => chroma.deltaE(c.hex, color.hex) < 20) ||
             palette.some(c => chroma.deltaE(c.hex, chroma(color.hex).desaturate(1.4)) < 18);
@@ -52,6 +54,12 @@ export class HomepageComponent implements OnInit {
         this.palettes = this.unfilteredPalettes;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.colorFilterSubscription) {
+      this.colorFilterSubscription.unsubscribe();
+    }
   }
 
   getPaletteParamId(palette: Color[]): string {
