784c7737201df9e769d9702ecada21f54e6b77de
adamfuhrer <adam.fuhrer@caseware.com>
2020-04-15 21:41:13 -0400

    scroll to top when home button in footer is clicked

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class FooterComponent {
+  onHomeClick() {
+    window.scrollTo(0, 0);
   }
-
 }
